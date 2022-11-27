5a03f88c0caa71d90c4ed98bfc174e1b990914f5
Ogu1208 <kdasunb6@gmail.com>
2022-11-27 20:25:04 +0900

    make wifiInfo

--- src/main/java/WifiService.java
+++ src/main/java/WifiService.java
@@ -1,2 +1,108 @@
 public class WifiService {
+
+    public List<WifiInfo> list() {
+// 5개
+        // 1. ip (domain)
+        // 2. port
+        // 3. 계정
+        // 4. 패스워드
+        // 5. 인스턴스
+
+        List<Member> memberList = new ArrayList<>();
+
+
+        String url = "jdbc:mariadb://192.168.0.21:3306/testdb3";
+        String dbUserId = "Ogu1208";
+        String dbPassword = "zerobase";
+
+        // 1. 드라이버 로드
+        // 2. 커넥션 객체 생성
+        // 3. 스테이트먼트 객체 생성
+        // 4. 쿼리 실행
+        // 5. 결과 수행
+        // 6. 객체 연결 해제 (close)
+
+        // 1. 드라이버 로드
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        // email, kakao, facebook
+        // sql injection 공격
+        String memberTypeValue = "email";
+
+        try {
+            // 2. 커넥션 객체 생성
+            connection = DriverManager.getConnection(url, dbUserId, dbPassword);
+            // 3. 스테이트먼트 객체 생성
+
+            // 4. 쿼리 실행
+            String sql = "select member_type, user_id, password, name " +
+                    " from member2 " +
+                    " where member_type = ? ";
+
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, memberTypeValue);   // parameter는 1부터 시작
+
+            rs = preparedStatement.executeQuery();
+
+            // 5. 결과 수행
+            while (rs.next()) {
+                String memberType = rs.getString("member_type");
+                String userId = rs.getString("user_id");
+                String password = rs.getString("password");
+                String name = rs.getString("name");
+
+
+                Member member = new Member();
+                member.setMemberType(memberType);
+                member.setUserId(userId);
+                member.setPassword(password);
+                member.setName(name);
+
+                memberList.add(member);
+
+                System.out.println(memberType + ", " + userId + ", " + password + ", " + name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 6. 객체 연결 해제 (close)
+            try {
+                if (rs != null && !rs.isClosed()) {
+                    rs.close();
+                    ;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (preparedStatement != null && !preparedStatement.isClosed()) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return memberList;
+
+    }
 }
---
+++ src/main/java/com/example/wifi_service/WifiInfo.java
@@ -0,0 +1,33 @@
+package com.example.wifi_service;
+
+public class WifiInfo {
+    private Double distance; // 거리
+
+    private String adminNumber; // 관리 번호
+
+    private String borough; // 자치구
+
+    private String wifiName; // wifi 네임
+
+    private String loadName; // 도로명 주소
+
+    private String detailAddress; // 상세 주소
+
+    private String installPosition; // 설치 위치
+
+    private String installType; // 설치 유형
+
+    private String installAgency; // 설치 기관
+
+    private String serviceType; // 서비스구분
+
+    private String netType; // 망종류
+
+    private Integer installYear; // 설치년도
+
+    private String inOutDoorType; // 실내외구분
+
+    private String wifiConnEnv; // wifi 접속 환경
+
+    private String locationDate; // 위치 날짜 정보
+}
